bc601d6bf5c8ee5a6c4ac2835f3fce17b971b18b
Ксения Власенко <vlasenkoka@alderasoft.ru>
2024-06-13 17:10:39 +0700

    Тестовый проект на next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1>123</h1>;
+};
+
+export default Page;
---
+++ src/app/test/page.tsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.scss";
+
+const Test = () => {
+  return <h1 className={styles.test}>456</h1>;
+};
+
+export default Test;
