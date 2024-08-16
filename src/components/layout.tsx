3cf4539f37d9abff5c46eee07cb6bc0edebbe0f2
k-s-e-n-n-i <ksu.xa.v.ya@gmail.com>
2024-08-16 19:05:35 +0700

    radix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,20 @@
 import { Header } from "@/components/Header";
 import styled from "../styles/app.module.scss";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <main className={styled.main}>
+export default function Layout({
+  children,
+  hasContainer = true,
+}: {
+  children: React.ReactNode;
+  hasContainer?: boolean;
+}) {
+  const content = (
+    <>
       <Header />
       <hr />
       <div className={styled.container}>{children}</div>
-    </main>
+    </>
   );
+
+  return hasContainer ? <main className={styled.main}>{content}</main> : content;
 }
