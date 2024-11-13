87482b70afcd23d18485df1c0701536e3b766d93
k-s-e-n-n-i <ksu.xa.v.ya@gmail.com>
2024-11-13 15:18:43 +0700

    css анимации для "гармошки"

--- src/pages/anim1.tsx
+++ src/pages/anim1.tsx
@@ -3,13 +3,21 @@ import { Sticky } from "@/components/Sticky/Sticky";
 
 const Anim1 = () => {
   return (
-    <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "20px" }}>
-      <Sticky height="5000px" containerWidth="calc(50% - 80px)" top="100px">
+    <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", gap: "20px" }}>
+      <p>qsap</p>
+      <p>js</p>
+      <p>css</p>
+
+      <Sticky height="5000px" containerWidth="calc(33% - 80px)" top="100px">
         <Harmonic list={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} type="qsap" />
       </Sticky>
 
-      <Sticky height="5000px" containerWidth="calc(50% - 80px)" top="100px">
+      <Sticky height="5000px" containerWidth="calc(33% - 80px)" top="100px">
         <Harmonic list={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} type="js" />
+      </Sticky>
+
+      <Sticky height="5000px" containerWidth="calc(33% - 80px)" top="100px">
+        <Harmonic list={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} type="css" />
       </Sticky>
     </div>
   );
